2c7d03df70943f855bf43c5e5955bf53c8432dbe
sergioviniciuss <sergioviniciuss@gmail.com>
2018-03-25 22:09:22 +0200

    Usando outra sintaxe para tratar a promise retornada pelo get do angular

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,10 +1,17 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http) {
     $scope.fotos = [];
     
-    var promise = $http.get("/v1/fotos");
-    promise.then(function(result) {
-        $scope.fotos = result.data;
-    }).catch(function(error) {
+    var promise = $http.get("/v1/fotos")
+    .success(function(fotos) {
+        $scope.fotos = fotos
+    })
+    .error(function(error) {
         console.log(error);
     })
+    // var promise = $http.get("/v1/fotos");
+    // promise.then(function(result) {
+    //     $scope.fotos = result.data;
+    // }).catch(function(error) {
+    //     console.log(error);
+    // })
 })
