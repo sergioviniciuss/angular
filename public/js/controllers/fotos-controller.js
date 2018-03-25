07728e008a27c2b194585ee94fa834adce1cd12d
sergioviniciuss <sergioviniciuss@gmail.com>
2018-03-25 21:58:47 +0200

    usando a diretiva ng-repeat e pegando dados da api usando a diretiva http do angular

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,6 +1,10 @@
-angular.module('alurapic').controller('FotosController', function($scope) {
-    $scope.foto = {
-        titulo: "Leao",
-        url: "http://www.fundosanimais.com/Minis/leao.jpg",
-    }
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
+    $scope.fotos = [];
+    
+    var promise = $http.get("/v1/fotos");
+    promise.then(function(result) {
+        $scope.fotos = result.data;
+    }).catch(function(error) {
+        console.log(error);
+    })
 })
